c0e08ac48289ed438477da79051241311cca4fa8
MmdNazim <miamdnazim@gmail.com>
2026-01-11 19:51:40 +0600

    create models and migration

---
+++ tasks/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=128)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)        #[NOTE: eikhane "auto_now_add" ei function tar kaj holo je jokhn ami data entry krbo tokhn ei function ta current time ta nibe.]
+    update_at = models.DateTimeField(auto_now=True)
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)     
+    update_at = models.DateTimeField(auto_now=True)
+
+
