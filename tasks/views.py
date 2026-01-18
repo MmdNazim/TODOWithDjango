0d88152ecb99329099dd316e41cc1a5b9c05ab7b
MmdNazim <miamdnazim@gmail.com>
2026-01-18 14:31:45 +0600

    soft working on templates

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from.models import Task
+
 # Create your views here.
+# crud = create, read, update, delete
+#NOTE: create equivalent to = Post, read = get, update = put/patch, delete ----> this all are use in rest api's.
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(
+        request, 
+        "tasks/base.html",
+        {"tasks": tasks}
+    )
+
