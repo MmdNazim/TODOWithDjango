8c637f04d41492b70ee5bb8b164daa95de776b11
MmdNazim <miamdnazim@gmail.com>
2026-01-19 18:57:20 +0600

    add new model and learning database relations

--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Task(models.Model):
+    Category = models.ForeignKey(Category, on_delete=models.SET_NULL)
     title = models.CharField(max_length=128)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)        #[NOTE: eikhane "auto_now_add" ei function tar kaj holo je jokhn ami data entry krbo tokhn ei function ta current time ta nibe.]
@@ -14,7 +21,6 @@ class Blog(models.Model):
     published = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)     
     update_at = models.DateTimeField(auto_now=True)
-
 
 
 
